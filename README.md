1a3f2e005b84417fd08fe558da8b329c9ecdfd2e
Parthas-Menethil <xywhpanda@gmail.com>
2013-10-26 21:38:52 -0400

    Update author information

---
+++ README.md
@@ -0,0 +1,7 @@
+Doubly Linked List (C++)
+================================
+Author:
+-------------
+Luke Jingwei Sun(Parthas Menethil)
+https://github.com/Parthas-Menethil
+http://blog.xparthas.com
--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,9 @@
+/*
+Doubly Linked List (C++)
+Luke Jingwei Sun(Parthas Menethil)
+https://github.com/Parthas-Menethil
+http://blog.xparthas.com
+*/
 #include "DoublyLinkedList.h"
 #include <iostream>
 using namespace std;
